6acd2490ea70a704f00ff1dc40f1ba8b753552f1
idcetim <javirofe96@gmail.com>
2023-09-03 19:36:38 +0200

    Add metamask connection and improve header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,50 @@
 import '../styles/header.css'
 import twitter from '../images/twitter.png'
+import { ethers } from 'ethers'
+import { useState } from 'react'
+
+declare global {
+  interface Window {
+    ethereum: any
+  }
+}
+
+interface IWalletInfo {
+  address?: string
+  balance?: string
+}
 const Header = () => {
+  const [walletInfo, setUserWalletInfo] = useState<IWalletInfo>({})
+  const connectionHandler = () => {
+    if (window.ethereum as any) {
+      try {
+        window.ethereum
+          .request({ method: 'eth_requestAccounts' })
+          .then((accounts: string) => {
+            accountChangeHandler(accounts[0])
+          })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+
+  const accountChangeHandler = (address: string) => {
+    window.ethereum
+      .request({ method: 'eth_getBalance', params: [address, 'latest'] })
+      .then((balance: string) => {
+        setUserWalletInfo({
+          address,
+          balance: ethers.formatEther(balance),
+        })
+      })
+  }
   return (
     <header className="header">
-      <img src={twitter} alt="twitter-icon" className='header-img' />
-      <button className="header-btn"> CONNECT WALLET </button>
+      <img src={twitter} alt="twitter-icon" className="header-img" />
+      <button className="header-btn" onClick={connectionHandler}>
+        {!walletInfo.address ? "CONNECT WALLET" : `${walletInfo?.address?.slice(0,6)}...${walletInfo?.address?.slice(36)}`}
+      </button>
     </header>
   )
 }
