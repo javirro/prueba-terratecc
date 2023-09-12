3cd8009619d41102d4756cfdae5f5f069be1d4a8
idcetim <javirofe96@gmail.com>
2023-09-12 14:11:40 +0200

    Fix big space between component and add correct hide navbar when is not hovered

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 import Header from './components/Header'
 import AsideNavbar from './components/AsideNavbar'
 import MainSection from './components/MainSection'
@@ -6,10 +6,11 @@ import Footer from './components/Footer'
 import './App.css'
 
 function App() {
+  const [isHovered, setIsHovered] = useState<boolean>(false)
   return (
     <main className="app-container">
-      <AsideNavbar />
-      <section className="app">
+      <AsideNavbar isHovered={isHovered} setIsHovered={setIsHovered} />
+      <section className={ !isHovered  ? 'middle-section-container' : 'middle-section-container-to-right' }>
         <Header />
         <MainSection />
         <Footer />
--- src/components/AsideNavbar.tsx
+++ src/components/AsideNavbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import logo from '../images/logo.png'
 import asideIcon from '../images/asideIcon.png'
 import asideIcon2 from '../images/asideIcon2.png'
@@ -6,30 +6,29 @@ import asideIcon3 from '../images/asideIcon3.png'
 import HoveredAside from './HoveredAside'
 import '../styles/asidenavbar.css'
 
-const AsideNavbar = () => {
-  const [isHovered, setIsHovered] = useState<boolean>(false);
+const AsideNavbar = ({ isHovered, setIsHovered }: {isHovered: boolean, setIsHovered: (s:boolean) => void}) => {
   const handleMouseEnter = () => {
     setIsHovered(true);
     // You can add your custom logic here when the mouse enters the image
   };
 
   const handleMouseLeave = () => {
-    //setIsHovered(false);
+    setIsHovered(false);
     // You can add your custom logic here when the mouse leaves the image
   };
   return (
 
-    <aside className="aside">
+    <aside className="aside"  onMouseLeave={handleMouseLeave}>
       <section className="aside-container">
           <img src={logo} alt="logo" className="aside-img" />
         <article className="icon-container">
-          <img src={asideIcon} alt="icon-1" className="aside-icon" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+          <img src={asideIcon} alt="icon-1" className="aside-icon" onMouseEnter={handleMouseEnter} />
         </article>
         <article  className="icon-container">
-          <img src={asideIcon2} alt="icon-2" className="aside-icon"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
+          <img src={asideIcon2} alt="icon-2" className="aside-icon"  onMouseEnter={handleMouseEnter} />
         </article>
         <article  className="icon-container">
-          <img src={asideIcon3} alt="icon-3" className="aside-icon" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+          <img src={asideIcon3} alt="icon-3" className="aside-icon" onMouseEnter={handleMouseEnter}  />
         </article>
       </section>
           {isHovered && <HoveredAside />}
